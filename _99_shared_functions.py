288cc44bea81a1067262840d603575176be3991f
Corey Chivers <corey.chivers@uphs.upenn.edu>
2020-04-16 21:59:04 -0400

    Cleanup depricated params

--- _99_shared_functions.py
+++ _99_shared_functions.py
@@ -17,7 +17,6 @@ outdir = f"{os.getcwd()}/output/"
 figdir = f"{os.getcwd()}/figures/"
 
 
-
 def write_txt(str, path):
     text_file = open(path, "w")
     text_file.write(str)
@@ -32,18 +31,14 @@ def sir(y, alpha, beta, gamma, nu, N):
     In = (alpha * E - gamma * I) + I
     Rn = gamma * I + R
 
-    if Sn < 0:
-        Sn = 0
-    if In < 0:
-        In = 0
-    if Rn < 0:
-        Rn = 0
-    if En < 0:
-        En = 0
     scale = N / (Sn + En + In + Rn)
     return Sn * scale, En * scale, In * scale, Rn * scale
 
+
 def reopenfn(day, reopen_day=60, reopen_speed=0.1):
+    """Starting on `reopen_day`, reduce contact restrictions
+    by `reopen_speed`*100%.
+    """
     if day < reopen_day:
         return 1.0
     else:
@@ -102,13 +97,13 @@ def qdraw(qvec, p_df):
                        val = getattr(sps, p_df.distribution.iloc[i]).ppf(*p))
             # does scipy not have a function to get the density from the quantile?
             p_pdf = (out['val'],) + p[1:]
-            out.update({"prob": getattr(sps, p_df.distribution.iloc[i]).pdf(*p_pdf)})        
+            out.update({"prob": getattr(sps, p_df.distribution.iloc[i]).pdf(*p_pdf)})
         outdicts.append(out)
     return pd.DataFrame(outdicts)
 
 def jumper(start, jump_sd):
     probit = sps.norm.ppf(start)
-    probit += np.random.normal(size = len(probit), scale = jump_sd)
+    probit += np.random.normal(size=len(probit), scale=jump_sd)
     newq = sps.norm.cdf(probit)
     return newq
 
@@ -120,9 +115,6 @@ def SIR_from_params(p_df):
     #
     n_hosp = int(p_df.val.loc[p_df.param == 'n_hosp'])
     incubation_days = float(p_df.val.loc[p_df.param == 'incubation_days'])
-    # n_infec = int(p_df.val.loc[p_df.param == 'n_infec'])
-    doubling_time = float(p_df.val.loc[p_df.param == 'doubling_time'])
-    soc_dist = float(p_df.val.loc[p_df.param == 'soc_dist'])
     hosp_prop = float(p_df.val.loc[p_df.param == 'hosp_prop'])
     ICU_prop = float(p_df.val.loc[p_df.param == 'ICU_prop'])
     vent_prop = float(p_df.val.loc[p_df.param == 'vent_prop'])
@@ -135,7 +127,7 @@ def SIR_from_params(p_df):
     logistic_k = float(p_df.val.loc[p_df.param == 'logistic_k'])
     logistic_L = float(p_df.val.loc[p_df.param == 'logistic_L'])
     logistic_x0 = float(p_df.val.loc[p_df.param == 'logistic_x0'])
-    beta = float(p_df.val.loc[p_df.param == 'beta']) # get beta directly rather than via doubling time
+    beta = float(p_df.val.loc[p_df.param == 'beta'])  # get beta directly rather than via doubling time
     nu = float(p_df.val.loc[p_df.param == 'nu']) + 1.0
 
     reopen_day, reopen_speed = 1000, 0.0
@@ -145,24 +137,21 @@ def SIR_from_params(p_df):
         reopen_speed = float(p_df.val.loc[p_df.param == 'reopen_speed'])
     #
     alpha = 1 / incubation_days
-    gamma = 1 / recovery_days  # , random_draw=random_draw)
-    doubling_time = doubling_time
-    intrinsic_growth_rate = 2 ** (1 / doubling_time) - 1
+    gamma = 1 / recovery_days
     total_infections = n_hosp / mkt_share / hosp_prop
-    # detection_prob = n_infec / total_infections
-    #beta = (intrinsic_growth_rate + alpha + gamma)
+
     n_days = 200
 
     # Offset by the incubation period to start the sim
     # that many days before the first hospitalization
     # Estimate the number Exposed from the number hospitalized
-    # on the first day of non-zero covid hospitalizations. 
+    # on the first day of non-zero covid hospitalizations.
     from scipy.stats import expon
     # Since incubation_days is exponential in SEIR, we start
     # the time `offset` days before the first hospitalization
     # We determine offset by allowing enough time for the majority
     # of the initial exposures to become infected.
-    offset = expon.ppf(0.99, 1 / incubation_days) # Enough time for 95% of exposed to become infected
+    offset = expon.ppf(0.99, 1 / incubation_days)  # Enough time for 95% of exposed to become infected
     offset = int(offset)
     #
     s, e, i, r = sim_sir(S=region_pop - total_infections,
@@ -183,9 +172,9 @@ def SIR_from_params(p_df):
     hosp_raw = hosp_prop
     ICU_raw = hosp_raw * ICU_prop  # coef param
     vent_raw = ICU_raw * vent_prop  # coef param
-    
-    ds = np.diff(i) + np.diff(r) # new infections is delta i plus delta r 
-    ds = np.array([0]+list(ds))
+
+    ds = np.diff(i) + np.diff(r)  # new infections is delta i plus delta r
+    ds = np.array([0] + list(ds))
     ds = ds[offset:]
 
     hosp = ds * hosp_raw * mkt_share
@@ -222,132 +211,10 @@ def SIR_from_params(p_df):
     output = dict(days=np.asarray(proj.day),
                   arr=np.asarray(proj)[:, 1:],
                   names=proj.columns.tolist()[1:],
-                  parms = p_df,
+                  parms=p_df,
                   s=s,
                   e=e,
                   i=i,
                   r=r,
                   offset=offset)
     return output
-    
-    
-#     output.update({'prior_prob':np.sum(np.log(np.array([output['probs'][i] for i in output['probs'].keys()])))})          
-
-
-
-# def sensitivity_wrapper(seed=8675309, random_draw=True, modparm=None, 
-#                         modval=None, output_SIR = False):
-#     np.random.seed(seed)
-#     # define a temporary parameter matrix in which the twiddled parameter is set as a constant
-#     p_df = copy.deepcopy(params)
-#     if modparm is not None:
-#         if type(modparm).__name__ == "str":
-#             assert modparm in p_df.param.tolist()
-#             p_df.loc[p_df['param'] == modparm, 'base'] = float(modval)
-#             p_df.loc[p_df['param'] == modparm, 'distribution'] = 'constant'
-#         elif type(modparm).__name__ == "list":
-#             assert(all([i in p_df.param.tolist() for i in modparm]))
-#             for i in range(len(modparm)):
-#                 p_df.loc[p_df['param'] == modparm[i], 'base'] = float(modval[i])
-#                 p_df.loc[p_df['param'] == modparm[i], 'distribution'] = 'constant'
-#     # define all of the parameters via calls to getparm
-#     recovery_days, rd_prob = getparm("recovery_days", p_df=p_df, random_draw = random_draw, get_prob = True)
-#     doubling_time, dt_prob = getparm("doubling_time", random_draw=random_draw, p_df=p_df, get_prob = True)
-#     soc_dist, sd_prob = getparm('soc_dist', random_draw=random_draw, p_df=p_df, get_prob = True)
-#     hosp_prop, hp_prob = getparm('hosp_prop', random_draw=random_draw, p_df=p_df, get_prob = True)
-#     ICU_prop, ip_prob = getparm('ICU_prop', random_draw=random_draw, p_df=p_df, get_prob = True)
-#     vent_prop, vp_prob = getparm('vent_prop', random_draw=random_draw, p_df=p_df, get_prob = True)
-#     hosp_LOS, hl_prob = getparm('hosp_LOS', random_draw=random_draw, p_df=p_df, get_prob = True)
-#     ICU_LOS, il_prob = getparm('ICU_LOS', random_draw=random_draw, p_df=p_df, get_prob = True)
-#     vent_LOS, vl_prob = getparm('vent_LOS', random_draw=random_draw, p_df=p_df, get_prob = True)
-#     #
-#     gamma = 1 / recovery_days  # , random_draw=random_draw)
-#     doubling_time = doubling_time
-#     intrinsic_growth_rate = 2 ** (1 / doubling_time) - 1
-#     total_infections = getparm('n_hosp', p_df=p_df) / \
-#                        getparm('mkt_share', p_df=p_df) / \
-#                        hosp_prop
-#     detection_prob = getparm('n_infec', p_df=p_df) / total_infections
-#     beta = (
-#                    intrinsic_growth_rate + gamma
-#            ) / getparm('region_pop', p_df=p_df) * (1 - soc_dist)
-#     n_days = 200
-
-#     s, i, r = sim_sir(S=getparm('region_pop', p_df=p_df),
-#                       I=getparm('n_infec', p_df=p_df) / detection_prob,
-#                       R=0,
-#                       beta=beta,
-#                       gamma=gamma,
-#                       n_days=n_days)
-#     if output_SIR == True:
-#         return np.vstack([s,i,r]).T
-#     hosp_raw = hosp_prop
-#     ICU_raw = hosp_raw * ICU_prop  # coef param
-#     vent_raw = ICU_raw * vent_prop  # coef param
-    
-#     ds = np.diff(s*-1)
-#     ds = np.array([0]+list(ds))
-
-#     hosp = ds * hosp_raw * getparm('mkt_share', p_df=p_df)
-#     icu = ds * ICU_raw * getparm('mkt_share', p_df=p_df)
-#     vent = ds * vent_raw * getparm('mkt_share', p_df=p_df)
-
-#     # make a data frame with all the stats for plotting
-#     days = np.array(range(0, n_days + 1))
-#     data_list = [days, hosp, icu, vent]
-#     data_dict = dict(zip(["day", "hosp_adm", "icu_adm", "vent_adm"], data_list))
-#     projection = pd.DataFrame.from_dict(data_dict)
-#     projection_admits = projection
-#     projection_admits["day"] = range(projection_admits.shape[0])
-#     # census df
-#     hosp_LOS_raw = hosp_LOS
-#     ICU_LOS_raw = ICU_LOS
-#     vent_LOS_raw = ICU_LOS_raw * vent_LOS  # this is a coef
-
-#     los_dict = {
-#         "hosp_census": hosp_LOS_raw,
-#         "icu_census": ICU_LOS_raw,
-#         "vent_census": vent_LOS_raw,
-#     }
-#     census_dict = {}
-#     for k, los in los_dict.items():
-#         census = (
-#                 projection_admits.cumsum().iloc[:-int(los), :]
-#                 - projection_admits.cumsum().shift(int(los)).fillna(0)
-#         ).apply(np.ceil)
-#         census_dict[k] = census[re.sub("_census", "_adm", k)]
-#     proj = pd.concat([projection_admits, pd.DataFrame(census_dict)], axis=1)
-#     proj = proj.fillna(0)
-    
-#     if random_draw is True:
-#         output = dict(days=np.asarray(proj.day),
-#                       arr=np.asarray(proj)[:, 1:],
-#                       names=proj.columns.tolist()[1:],
-#                       parms = dict(doubling_time = doubling_time,
-#                                    soc_dist = soc_dist,
-#                                    hosp_prop = hosp_prop,
-#                                    ICU_prop = ICU_prop,
-#                                    vent_prop = vent_prop,
-#                                    hosp_LOS = hosp_LOS,
-#                                    ICU_LOS = ICU_LOS,
-#                                    vent_LOS = vent_LOS,
-#                                    recovery_days = recovery_days),
-#                       probs = dict(recovery_days = rd_prob,
-#                                    doubling_time = dt_prob,
-#                                    soc_dist = sd_prob ,
-#                                    hosp_prop = hp_prob ,
-#                                    ICU_prop = ip_prob, 
-#                                    vent_prop = vp_prob,
-#                                    hosp_LOS = hl_prob,
-#                                    ICU_LOS = il_prob,
-#                                    vent_LOS = vl_prob))
-#         output.update({'prior_prob':np.sum(np.log(np.array([output['probs'][i] for i in output['probs'].keys()])))})          
-#         return output
-#     else:
-#         return proj
-
-
-
-
-
-
